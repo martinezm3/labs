60a355352289786877e1d5616004bc0855d299ba
martinezm3 <54373675+martinezm3@users.noreply.github.com>
2019-08-22 21:19:54 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,15 @@
+public class Hello {
+	public static void printHeading() {
+	 System.out.println("Miguel Martinez");
+     System.out.println("Hello Lab");
+     System.out.println("Intro to Java");
+     System.out.println("08/21/2019");
+     System.out.println("CMSC 255 Section 2");
+     System.out.println();
+ }
+
+	public static void main(String[] args) {
+	 printHeading();
+	 System.out.println("Hello");
+	}
+}
